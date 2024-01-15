21cd6ac6c16417e81f151fbb8cc82de78b5446c1
Icetail <51836525+Icetail1@users.noreply.github.com>
2024-01-15 09:16:08 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// script.js
+
+function uploadImage() {
+  var input = document.getElementById('imageInput');
+  var resultDiv = document.getElementById('result');
+
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      var imageSrc = e.target.result;
+
+      // 调用 OCR 函数
+      recognizeText(imageSrc, function (text) {
+        resultDiv.innerHTML = "<p>识别结果:</p><p>" + text + "</p>";
+      });
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function recognizeText(imageSrc, callback) {
+  // 使用 Tesseract.js 进行文字识别
+  Tesseract.recognize(
+    imageSrc,
+    'eng', // 语言，这里是英语
+    { logger: info => console.log(info) } // 可选参数，用于输出日志
+  ).then(({ data: { text } }) => {
+    callback(text);
+  });
+}
